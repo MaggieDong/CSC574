622be31ed42b0f3d77c35836ebf44ed6c5434a8d
MaggieDong <tianqing705@gmail.com>
2015-11-15 17:30:58 -0500

    encrypt pwd and message then write them in a file

--- encryptMes.sh
+++ encryptMes.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 sessionPsw=$1
 # echo $sessionPsw
-openssl enc -aes-256-cbc -base64 -in message.txt -out sendFile.txt -pass pass:sessionPsw
-openssl enc -d -aes-256-cbc -base64 -in sendFile.txt -out sendFileout.txt -pass pass:sessionPsw 
+openssl enc -aes-256-cbc -base64 -in sessionFile.txt -out encryptedFile.txt -pass file:./publicKey.txt
+echo >> encryptedFile.txt 
+rm sessionFile.txt
+openssl enc -aes-256-cbc -base64 -in message.txt -out encryptedMsg.txt -pass pass:sessionPsw
+cat encryptedMsg.txt >> encryptedFile.txt 
+# rm message.txt
+
+openssl enc -d -aes-256-cbc -base64 -in encryptedMsg.txt -out original.txt -pass pass:sessionPsw
