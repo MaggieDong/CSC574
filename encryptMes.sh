d2c3de8d30858032de13ba7dbdfeebf97634a0a1
MaggieDong <tianqing705@gmail.com>
2015-11-15 20:40:01 -0500

    finish most of decryption part, divide into two functions

--- encryptMes.sh
+++ encryptMes.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
+#session pwd encrypted under target's public key
 sessionPsw=$1
-# echo $sessionPsw
 openssl enc -aes-256-cbc -base64 -in sessionFile.txt -out encryptedFile.txt -pass file:./publicKey.txt
 echo >> encryptedFile.txt 
 rm sessionFile.txt
+
+#message encrypted under session pwd above
 openssl enc -aes-256-cbc -base64 -in message.txt -out encryptedMsg.txt -pass pass:sessionPsw
 cat encryptedMsg.txt >> encryptedFile.txt 
-# rm message.txt
+echo >> encryptedFile.txt
+rm message.txt
+rm encryptedMsg.txt
 
-openssl enc -d -aes-256-cbc -base64 -in encryptedMsg.txt -out original.txt -pass pass:sessionPsw
+# openssl enc -d -aes-256-cbc -base64 -in encryptedMsg.txt -out original.txt -pass pass:sessionPsw
+
+#signature of above content
+openssl dgst -sha1 -sign RSA.pem -out encryptedFile.txt.sha1 encryptedFile.txt
+echo "encryptedFile.txt.sha1" >> encryptedFile.txt 
+
+#Format and send
+echo "-----BEGIN CSC474 MESSAGE-----" >> sendFile.txt
+cat encryptedFile.txt >> sendFile.txt
+echo "-----END CSC474 MESSAGE-----" >> sendFile.txt
+
+
+
+# openssl dgst -sha1 -verify public.pem -signature encryptedFile.txt.sha1 encryptedFile.txt
