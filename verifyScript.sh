39c5c6e072048c97c3f95b3b8c68cb12410afecb
yshang3 <yshang3@ncsu.edu>
2015-11-27 23:16:43 -0500

    obtain target's public key

--- verifyScript.sh
+++ verifyScript.sh
@@ -5,5 +5,6 @@ signedFN=$unsignedFN".sha1"
 echo $signedFN
 echo $unsignedFN
 
+openssl x509 -in pubCert.pem -pubkey -noout > public.pem
 openssl dgst -sha1 -verify public.pem -signature $signedFN $unsignedFN
 # openssl dgst -sha1 -verify public.pem -signature encryptedFile.txt.sha1 encryptedFile.txt
